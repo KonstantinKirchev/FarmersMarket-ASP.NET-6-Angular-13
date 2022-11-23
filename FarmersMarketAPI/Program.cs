4d1413bce3c4ad3f5ca5e8b3916321662e514081
Konstantin Kirchev <konstantin1230@yahoo.com>
2022-11-23 17:03:53 +0200

    register some services and middlewares

--- FarmersMarketAPI/Program.cs
+++ FarmersMarketAPI/Program.cs
@@ -1,4 +1,22 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using FarmersMarketAPI.Data;
+using FarmersMarketAPI.Models.EntityModels;
+using Microsoft.AspNetCore.Cors;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        //options.JsonSerializerOptions.WriteIndented = true;
+        //options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    });
 
 // Add services to the container.
 
@@ -6,6 +24,52 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+options.AddPolicy(name: "AngularPolicy",
+cfg => {
+    cfg.AllowAnyHeader();
+    cfg.AllowAnyMethod();
+    cfg.WithOrigins(builder.Configuration["AllowedCORS"]);
+}));
+
+// Add ApplicationDbContext and SQL Server support
+builder.Services.AddDbContext<FarmersMarketDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add ASP.NET Core Identity support
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 8;
+})
+.AddEntityFrameworkStores<FarmersMarketDbContext>();
+
+// Add Authentication services & middlewares
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        RequireExpirationTime = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.
+    GetBytes(builder.Configuration["JwtSettings:SecurityKey"]))
+    };
+});
+
+//builder.Services.AddScoped<JwtHandler>();
 
 var app = builder.Build();
 
@@ -17,9 +81,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
+app.UseCors("AngularPolicy");
 app.MapControllers();
 
 app.Run();
+
