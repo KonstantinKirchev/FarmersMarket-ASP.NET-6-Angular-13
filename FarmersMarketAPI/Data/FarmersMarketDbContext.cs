4c99e2d49056f0d9d2c8241f282fcdea725a676c
Konstantin Kirchev <konstantin1230@yahoo.com>
2022-11-23 17:02:14 +0200

    Added DB context and repository unit of work

---
+++ FarmersMarketAPI/Data/FarmersMarketDbContext.cs
@@ -0,0 +1,45 @@
+﻿namespace FarmersMarketAPI.Data
+{
+    using FarmersMarketAPI.Models.EntityModels;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class FarmersMarketDbContext : IdentityDbContext<User>
+    {
+        public FarmersMarketDbContext(DbContextOptions<FarmersMarketDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Farm> Farms { get; set; }
+
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+
+        public DbSet<ShoppingCartProduct> ShoppingCartProducts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ShoppingCartProduct>().HasKey(q =>
+                new
+                {
+                    q.ShoppingCartId,
+                    q.ProductId
+                });
+
+            builder.Entity<ShoppingCartProduct>()
+                .HasOne(t => t.ShoppingCart)
+                .WithMany(t => t.ShoppingCartProducts)
+                .HasForeignKey(t => t.ShoppingCartId);
+
+            builder.Entity<ShoppingCartProduct>()
+                .HasOne(t => t.Product)
+                .WithMany(t => t.ShoppingCartProducts)
+                .HasForeignKey(t => t.ProductId);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ FarmersMarketAPI/Data/UnitOfWork/IFarmersMarketData.cs
@@ -0,0 +1,22 @@
+﻿namespace FarmersMarketAPI.Data.UnitOfWork
+{
+    using FarmersMarketAPI.Data.Repositories;
+    using FarmersMarketAPI.Models.EntityModels;
+
+    public interface IFarmersMarketData
+    {
+        IRepository<User> Users { get; }
+
+        IRepository<Category> Categories { get; }
+
+        IRepository<Farm> Farms { get; }
+
+        IRepository<Product> Products { get; }
+
+        IRepository<ShoppingCart> ShoppingCarts { get; }
+
+        IRepository<ShoppingCartProduct> ShoppingCartProducts { get; }
+
+        int SaveChanges();
+    }
+}
