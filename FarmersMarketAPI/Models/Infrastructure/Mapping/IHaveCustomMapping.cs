737c2b9ef0ebf0de7e4b5c96d8a82ed690599d14
Konstantin Kirchev <konstantin1230@yahoo.com>
2022-11-23 17:00:34 +0200

    Added Automapper

---
+++ FarmersMarketAPI/Models/Infrastructure/Mapping/IHaveCustomMapping.cs
@@ -0,0 +1,9 @@
+﻿namespace FarmersMarketAPI.Models.Infrastructure.Mapping
+{
+    using AutoMapper;
+
+    public interface IHaveCustomMapping
+    {
+        void ConfigureMapping(Profile mapper);
+    }
+}
---
+++ FarmersMarketAPI/Models/Infrastructure/Mapping/IMapFrom.cs
@@ -0,0 +1,6 @@
+﻿namespace FarmersMarketAPI.Models.Infrastructure.Mapping
+{
+    public interface IMapFrom<TModel>
+    {
+    }
+}
