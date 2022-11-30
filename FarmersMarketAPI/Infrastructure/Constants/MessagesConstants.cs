4d2e8b49ad329a330f19be1ee86fd315c81106d2
Konstantin Kirchev <konstantin1230@yahoo.com>
2022-11-30 14:55:17 +0200

    Added app constants

---
+++ FarmersMarketAPI/Infrastructure/Constants/MessagesConstants.cs
@@ -0,0 +1,25 @@
+﻿namespace FarmersMarketAPI.Infrastructure.Constants
+{
+    public static class MessagesConstants
+    {
+        // Success messages
+        public const string CreateFarmSuccessMessage = "You have successfully created a farm.";
+        public const string EditFarmSuccessMessage = "You have successfully edited a farm.";
+        public const string DeleteFarmSuccessMessage = "You have successfully deleted a farm.";
+        public const string CreateCategorySuccessMessage = "You have successfully created a category.";
+        public const string EditCategorySuccessMessage = "You have successfully edited a category.";
+        public const string DeleteCategorySuccessMessage = "You have successfully deleted a category.";
+        public const string CreateProductSuccessMessage = "You have successfully created a product.";
+        public const string EditProductSuccessMessage = "You have successfully edited a product.";
+        public const string DeleteProductSuccessMessage = "You have successfully deleted a product.";
+        public const string AddProductToCartSuccessMessage = "Product added to the shopping cart.";
+        public const string RemoveProductFromCartSuccessMessage = "Product removed from the shopping cart.";
+        public const string TheOrderWasSuccessfullyPlaced = "The order was placed successfully";
+
+        // Error messages
+        // TO-DO
+
+        // Info messages
+        // TO-DO
+    }
+}
---
+++ FarmersMarketAPI/Infrastructure/Constants/WebConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace FarmersMarketAPI.Infrastructure.Constants
+{
+    public static class WebConstants
+    {
+        public const string AdminRole = "Admin";
+
+        public static string ManagerRole = "Manager";
+    }
+}
